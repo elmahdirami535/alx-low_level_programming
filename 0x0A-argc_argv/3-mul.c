43a260fe692a9a5d9f88b595cbb5e99f81e2d89a
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-11-07 06:46:53 -0800

    I add the project I missed

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+}
